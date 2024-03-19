f7e51a0e22d5c2870e7ca786ee6fb0b7027afeac
heyyrahul <rkkamat9213@gmail.com>
2024-03-19 23:11:30 +0530

    final_push

--- Frontend/src/client.jsx
+++ Frontend/src/client.jsx
@@ -2,7 +2,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/', 
+  uri: 'https://mysterious-bear-cloak.cyclic.app/', 
   cache: new InMemoryCache()
 });
 
